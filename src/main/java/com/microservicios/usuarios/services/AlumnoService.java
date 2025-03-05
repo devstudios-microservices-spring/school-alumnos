c61b2f96da05bbecc45da4f2123f5d992e32cc05
DevCompleteStudios <jorge.yael333@gmail.com>
2025-03-05 17:18:15 -0600

    Agregando transactional en los metodos de los servicios

--- src/main/java/com/microservicios/usuarios/services/AlumnoService.java
+++ src/main/java/com/microservicios/usuarios/services/AlumnoService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.microservicios.usuarios.models.Alumno;
 import com.microservicios.usuarios.repositories.IAlumnoRepository;
@@ -20,21 +21,25 @@ public class AlumnoService implements IAlumnoService {
 
 
     @Override
+    @Transactional(readOnly = true)
     public List<Alumno> findAll() {
         return repository.findAll();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Optional<Alumno> findById(Long id) {
         return repository.findById(id);
     }
 
     @Override
+    @Transactional(readOnly = false)
     public Alumno save(Alumno alumno) {
         return repository.save(alumno);
     }
 
     @Override
+    @Transactional(readOnly = false)
     public void deleteById(Long id) {
         repository.deleteById(id);
     }
